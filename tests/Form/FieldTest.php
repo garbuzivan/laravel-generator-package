276b64a947393667f647e975d14aaa5da392b2eb
ivangarbuz <garbuzivan90@ya.ru>
2021-03-31 08:58:57 +0300

    Update

--- tests/Form/FieldTest.php
+++ tests/Form/FieldTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace GarbuzIvan\LaravelGeneratorPackage\Test\Form;
 
+use Exception;
 use GarbuzIvan\LaravelGeneratorPackage\Facades\Field;
 use PHPUnit\Framework\TestCase;
 
@@ -11,7 +12,20 @@ class FieldTest extends TestCase
 {
     public function testFieldInit()
     {
-        Field::text('title', 'Title');
-        $this->assertIsBool(true);
+        $field = Field::text('title', 'Title');
+        $this->assertIsBool($field->getColumn() == 'title');
+        $this->assertIsBool($field->getLabel() == 'Title');
+    }
+
+    public function testFieldInitExceptionTypeColumn()
+    {
+        Field::text(123, 'Title');
+        $this->expectException(Exception::class);
+    }
+
+    public function testFieldInitExceptionMaskColumn()
+    {
+        Field::text('проверка', 'Title');
+        $this->expectException(Exception::class);
     }
 }
